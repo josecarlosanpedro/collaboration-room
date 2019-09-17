126d4dba2bebbd83c4f6e772a8394ab4d15b6226
unknown <josecarlosanpedro>
2019-09-17 10:54:33 +0800

    fix comment and data

--- src/modules/Classroom/components/Chatbox/index.jsx
+++ src/modules/Classroom/components/Chatbox/index.jsx
@@ -1,54 +1,117 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import Input from 'antd/lib/input';
+import Popover from 'antd/lib/popover';
 import Icon from 'antd/lib/icon';
-import Button from 'antd/lib/button';
+import Button from 'antd/lib/button'
 import { messageData } from '../../helpers/fakeData';
 import sendIcon from '../../../../images/send_icon.png';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import firebase from 'firebase';
+import uuid from 'uuid/v1';
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
-const Chatbox = () => (
-  <section className="chatbox-section">
-    <h1 className="title">
-      <Icon type="mail" /> CHATBOX
+
+const Chatbox = props => {
+  const [message, setMessage] = useState('');
+  const [conversationList, setConversationList] = useState([]);
+
+  useEffect(() => {
+    const conversation = firebase.database()
+      .ref()
+      .child(`chat-box/${props.room}`);
+    conversation.on('value', conversationSuccess, conversationError);
+  }, []);
+
+  const conversationSuccess = data => {
+
+    if (data.val() != null) {
+
+      setConversationList(Object.values(data.val()))
+    }
+  }
+  const conversationError = data => {
+    console.log('error', data)
+  }
+  const handleSend = () => {
+    if (message !== "") {
+      firebase.database()
+        .ref()
+        .child(`chat-box/${props.room}/${uuid()}`)
+        .set({
+          messageText: message,
+          userId: props.user._id,
+          firstName: props.user.firstName,
+          lastName: props.user.lastName,
+          logo: props.user.logo || 'https://icon-library.net/images/no-user-image-icon/no-user-image-icon-27.jpg',
+          timestamp: firebase.database.ServerValue.TIMESTAMP
+        })
+        .then(() => {
+          setMessage('')
+          console.log('nice')
+        });
+    }
+
+  }
+  const handleMessage = e => {
+    setMessage(e.target.value)
+  }
+  const emojiClick = e => {
+    setMessage(message + e.native)
+  }
+  console.log(props.user, 'props.user')
+  return (
+    <section className="chatbox-section">
+      <h1 className="title">
+        <Icon type="mail" /> CHATBOX
     </h1>
-    <div className="content">
-      <div className="message-container">
-        {messageData.map(item => {
-          const isFromUser = item.from === 'user';
-          return (
-            <div
-              className={classnames('message-item', {
-                'message-item-user': isFromUser,
-              })}
-            >
+      <div className="content">
+        <ScrollToBottom className="message-container">
+          {conversationList.map(item => {
+            const isFromUser = item.userId === props.user._id;
+            return (
               <div
-                className={classnames('message', {
-                  'message-user': isFromUser,
+                className={classnames('message-item', {
+                  'message-item-user': isFromUser,
                 })}
               >
-                {item.message}
+                <div
+                  className={classnames('message', {
+                    'message-user': isFromUser,
+                  })}
+                >
+                  {item.messageText}
+                </div>
+                <div className="sent-details">
+                  <span className="sender-name">
+                    {!isFromUser && item.firstName}
+                  </span>
+                  {/* <span className="time"> {item.timestamp}</span> */}
+                </div>
               </div>
-              <div className="sent-details">
-                <span className="sender-name">
-                  {!isFromUser && item.senderName}
-                </span>
-                <span className="time"> {item.time}</span>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </ScrollToBottom>
+        <div className="input-text-container">
+          <Input.TextArea
+            className="input-text"
+            placeholder="Type a message..."
+            value={message}
+            onChange={handleMessage}
+          />
+          <Popover placement="topRight" content={<Picker onSelect={emojiClick} />} trigger="click">
+            <Button>TL</Button>
+          </Popover>
+          <Button onClick={handleSend} className="send-button">
+            <img src={sendIcon} alt="send" className="send-icon" />
+          </Button>
+        </div>
       </div>
-      <div className="input-text-container">
-        <Input.TextArea
-          className="input-text"
-          placeholder="Type a message..."
-        />
-        <Button className="send-button">
-          <img src={sendIcon} alt="send" className="send-icon" />
-        </Button>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  )
+
+}
+
 
 export default Chatbox;
--- src/modules/Classroom/components/Documents/index.jsx
+++ src/modules/Classroom/components/Documents/index.jsx
@@ -1,26 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'antd/lib/icon';
 import Button from 'antd/lib/button';
+import firebase from "firebase";
+import ScrollToBottom from 'react-scroll-to-bottom';
+import uuid from 'uuid/v1';
+import FileBase64 from 'react-file-base64';
 import { documentData } from '../../helpers/fakeData';
 
-const Documents = () => (
-  <section className="documents-section">
-    <h1 className="title">
-      <Icon type="book" /> DOCUMENTS
+const Documents = props => {
+  const [documentList, setDocumentsList] = useState([]);
+
+  useEffect(() => {
+    const documents = firebase.database()
+      .ref('documents')
+      .child(`${props.room}`);
+    documents.on('value', documentsSuccess, documentsError);
+  }, []);
+  const documentsSuccess = data => {
+    if (data.val() != null) {
+      setDocumentsList(Object.values(data.val()))
+    }
+  }
+  const documentsError = data => {
+    console.log('error', data)
+  }
+
+  const getFiles = files => {
+    console.log(files)
+    files.map(file => {
+      const base64 = file.base64.split(',')
+      const storageRef = firebase.storage().ref('classroom/documents').child(file.name)
+      storageRef
+        .putString(base64[1], 'base64', { contentType: file.type }).then(result => {
+          return storageRef.getDownloadURL().then(downloadURL => {
+            const data = {
+              url: downloadURL,
+              file
+            }
+            saveFiles(data)
+          });
+        }).catch(error => console.log(error))
+    })
+  }
+  const saveFiles = data => {
+    console.log(data, 'data')
+    firebase.database()
+      .ref('documents')
+      .child(`${props.room}/${uuid()}`)
+      .set({
+        url: data.url,
+        fileName: data.file.name,
+        type: data.file.type
+      })
+      .then(() => {
+        console.log('success')
+      });
+  }
+  return (
+    <section className="documents-section">
+      <h1 className="title">
+        <Icon type="book" /> DOCUMENTS
     </h1>
-    <div className="content-container">
-      <div className="contents">
-        {documentData.map(item => (
-          <a href={item.path} target="_blank" rel="noopener noreferrer">
-            <Button className="item" key={item.id}>
+      <ScrollToBottom className="content-container">
+        <div className="contents">
+          {documentList.map((item, index) => (
+            <Button className="item" href={item.url} target="_blank" key={item.id}>
               <Icon type="file-text" className="item-icon" />
               <span className="item-title">{item.fileName}</span>
             </Button>
-          </a>
-        ))}
-      </div>
-    </div>
-  </section>
-);
+          ))}
+          <FileBase64
+            multiple={true}
+            onDone={getFiles} />
+        </div>
+      </ScrollToBottom>
+    </section>
+  )
+}
 
 export default Documents;
